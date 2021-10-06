c8d2fdba65df0999e43d8aa84edd06934aae0bcc
Jonas Brasil <69374413+jonascostabrasil@users.noreply.github.com>
2021-10-05 21:36:19 -0300

    Update volatilidade_dash.py

--- volatilidade_dash.py
+++ volatilidade_dash.py
@@ -73,10 +73,9 @@ st.write(currency.tail(10))
 
 #Grafico
 st.title('Volatility')
-fig = sns.displot(currency, x='Volatilidade_Acumulada',bins=10)
-fig.update_layout(
-    yaxis_title='Taxa'
-)
+fig = sns.displot(currency, x='Volatilidade_Acumulada',bins=10), plt.axvline(currency['Volatilidade_Acumulada'].quantile(.95),color='r',linewidth=4,ls='--')
+
+
 st.pyplot(fig)
 
 st.markdown('97%')
